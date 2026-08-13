c80bc45b500667821a26f28d0dd73d3d423e5dea
woyoabsen-glitch <woyoabsen@gmail.com>
2026-08-13 11:22:06 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -8,7 +8,7 @@
  * ========================================================= */
 
 const CONFIG = {
-  API_URL: 'GANTI_DENGAN_URL_WEB_APP_ANDA'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyThXG-e88ZOMLXznF9V2BuS-dUF4jjTFlAiOr9meHkZdrvap8sORf98sbZiGT9E-VeAg/exec'
 };
 
 const Session = {
